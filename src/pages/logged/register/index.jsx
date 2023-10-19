071324397d0f3d9f8e42d1a1889c2071a1523956
MATHEUS BECKER <matheus_becker@estudante.sesisenai.org.br>
2023-10-19 11:51:44 -0300

    a

--- src/pages/logged/register/index.jsx
+++ src/pages/logged/register/index.jsx
@@ -2,25 +2,27 @@ export default () => {
 
   return (
     <div className="h-[92%] flex justify-center items-center">
-      <div className="w-[70%] h-[90%] bg-gray-400 flex items-center flex-col">
+      <div className="w-[70%] h-[90%] flex items-center flex-col">
         <h1>Register</h1>
         <div className="w-full h-full flex items-center flex-col">
           <div className="divRegister">
             <p>Username</p>
-            <input type="text" />
+            <input className=" w-full px-3 py-2 border border-gray-400 rounded-sm focus:outline-none focus:ring focus:border-blue-300" type="text" />
           </div>
           <div className="divRegister">
             <p>Username</p>
-            <input type="text" />
+            <input className=" w-full px-3 py-2 border border-gray-400 rounded-sm focus:outline-none focus:ring focus:border-blue-300" type="text" />
           </div>
           <div className="divRegister">
             <p>Username</p>
-            <input type="text" />
+            <input  className=" w-full px-3 py-2 border border-gray-400 rounded-sm focus:outline-none focus:ring focus:border-blue-300"type="text" />
           </div>
           <div className="divRegister">
             <p>Username</p>
-            <input type="text" />
+            <input className=" w-full px-3 py-2 border border-gray-400 rounded-sm focus:outline-none focus:ring focus:border-blue-300" type="text" />
           </div>
+
+          <button className="h-[10%] w-2/6">register</button>
         </div>
       </div>
 
